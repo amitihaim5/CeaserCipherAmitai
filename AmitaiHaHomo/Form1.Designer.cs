44cb70fd4f82dbfefd7d918bbb1dbacd9a83d72e
amitihaim5 <136345875+amitihaim5@users.noreply.github.com>
2023-06-12 16:54:45 +0300

    Add files via upload

---
+++ AmitaiHaHomo/Form1.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace AmitaiHaHomo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.TextBox encryptedText;
+        private System.Windows.Forms.Button encryptedBtn;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.encryptedText = new System.Windows.Forms.TextBox();
+            this.encryptedBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // encryptedText
+            // 
+            this.encryptedText.Location = new System.Drawing.Point(60, 169);
+            this.encryptedText.Name = "encryptedText";
+            this.encryptedText.Size = new System.Drawing.Size(246, 20);
+            this.encryptedText.TabIndex = 0;
+            this.encryptedText.Text = "PLS enter the encrypted text";
+            this.encryptedText.TextChanged += new System.EventHandler(this.EncryptedText_TextChanged);
+            // 
+            // encryptedBtn
+            // 
+            this.encryptedBtn.Location = new System.Drawing.Point(114, 252);
+            this.encryptedBtn.Name = "encryptedBtn";
+            this.encryptedBtn.Size = new System.Drawing.Size(144, 52);
+            this.encryptedBtn.TabIndex = 1;
+            this.encryptedBtn.Text = "Decrypt";
+            this.encryptedBtn.UseVisualStyleBackColor = true;
+            this.encryptedBtn.Click += new System.EventHandler(this.EncryptedBtn_Click);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(383, 385);
+            this.Controls.Add(this.encryptedBtn);
+            this.Controls.Add(this.encryptedText);
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+    }
+}
+
---
+++ AmitaiHaHomo/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+
+using System;
+using System.IO;
+using System.Windows.Forms;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace AmitaiHaHomo
+{
+    public partial class Form1 : Form
+    {
+        private NetSpell.SpellChecker.Dictionary.WordDictionary oDict = new NetSpell.SpellChecker.Dictionary.WordDictionary();
+        private NetSpell.SpellChecker.Spelling oSpell = new NetSpell.SpellChecker.Spelling();
+        public Form1()
+        {
+            InitializeComponent();
+
+            oDict.DictionaryFile = "en-US.dic";
+            oDict.Initialize();
+            oSpell.Dictionary = oDict;
+
+        }
+
+        private void Check(string word)
+        {
+            char space = ' ';
+            string [] arr = new string[999];
+            int spaceCounter = 0, wordCounter = 0, i = 0;
+            foreach (char character in word)
+            {
+                if (character != space)
+                {
+                    arr[i] += character.ToString();
+                }
+                else
+                {
+                    i++;
+                    spaceCounter++;
+                }
+            }
+            for (int j = 0; i < arr.Length; i++)
+            {
+                if (oSpell.TestWord(arr[j]))
+                {
+                    wordCounter++;
+                }
+            }
+            if (spaceCounter / 2 < wordCounter)
+            {
+                MessageBox.Show(word);
+            }
+        }
+        private void EncryptedBtn_Click(object sender, EventArgs e)
+        {
+            int num = 0;
+            string name = encryptedText.Text;
+            string letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            for (int key = 0; key < letters.Length; key++)
+            {
+                string translated = "";
+                foreach (char symbol in name)
+                {
+                    if (char.IsLetter(symbol))
+                    {
+                        num = letters.IndexOf(symbol);
+                        num = (num - key + letters.Length) % letters.Length;
+
+                        if (num < 0)
+                            num = num + letters.Length;
+
+                        translated += letters[num];
+                    }
+                    else
+                    {
+                        translated += symbol;
+                    }
+                }
+                Check(translated.ToLower());
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void EncryptedText_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_BackColorChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
